fc1a414e21c3aded61a357ef32b24fdde0adac8e
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2023-03-31 22:14:27 -0700

    Adds more routes, user password auth.

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/Users')
+const bcrypt = require('bcrypt')
 
 //POST /auth/register - REGISTER new User
 router.get('/register', async (req, res) => {
@@ -11,6 +12,41 @@ router.get('/register', async (req, res) => {
 
     await user.save()
     res.send(user)
+ })
+router.post('/register', async (req, res) => {
+    try {
+        //hash new password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(user.password, salt)
+
+        //create new user
+        const user = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        //save user and respond
+        const savedUser = await user.save()
+        res.status(200).json(savedUser)
+    } catch(error) {
+        console.log(error, "register post route")
+    }
+
+ })
+
+ //POST /auth/login - LOGIN User
+ router.post('/login', async (req, res) => {
+    try {
+    const user = await User.findOne({email: req.body.email})
+    !user && res.status(404).json("user not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
+    } catch(error) {
+        console.log(error, "login post route")
+    }
  })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    
- })
+//updatet user
+router.put('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+            
+            } catch (error) {
+                res.status(500).json(error)
+            }
+        } else {
+            res.status(403).json("You can update only your account")
+        }
+}
+
+//delete user
+
+//get a user
+
+//follow a user
+
+//unfollow a user
 
 module.exports = router;
